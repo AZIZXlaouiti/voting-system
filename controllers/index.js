ae3c4a4f05bebbbd7ed236a0e8188c46462b4b25
AZIZXlaouiti <azizrocke@gmail.com>
2023-03-16 16:20:35 -0400

    adding db configuration

--- controllers/index.js
+++ controllers/index.js
@@ -7,7 +7,7 @@ let express = require('express'),
   * Second route will handle the API calls.
 */
 
-router.use('/', require('./home')) // using home.js
+router.use('/', require('./home')) // using home.js 
 router.use('/polls', require('./polls')) // using polls.js 
 
 module.exports = router
---
+++ models/db.js
@@ -0,0 +1,17 @@
+"use strict"
+let rethinkdb = require('rethinkdb'),
+    async = require('async')
+
+class db {
+    setupDb(){
+
+    }
+    connectToRethinkDbServer(callback){
+
+    }
+    connectToDb(callback){
+
+    }
+}
+
+module.exports = db ;
