acb7e7402cbe8646c51396038da11b835c8f92cb
AZIZXlaouiti <azizrocke@gmail.com>
2023-03-16 18:06:31 -0400

    finished setting up rethinkdb connection

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // telling app.js to use controller folder as the route
 app.use(require('./controllers'))
 
-app.get('/' , function(req , res){
-    res.send('ok')
-})
+
 
 http.listen(3000, function(req , res){
     console.log('listening on port 3000')
--- models/db.js
+++ models/db.js
@@ -1,16 +1,67 @@
 "use strict"
-let rethinkdb = require('rethinkdb'),
+let r = require('rethinkdb'),
     async = require('async')
 
 class db {
-    setupDb(){
+    setupDb(){ 
+        let self = this
+        // array of tasks
+        async.waterfall([ 
+            /**
+             * connnect to db
+             * create db 
+             * create table
+             */
+                function(callback){
+                    self.connectToRethinkDbServer(function(err , conn){
+                        if (err){
+                            return callback(true, 'Error in connection RethinkDB')
+                        }
+                        callback(null, conn)
+                    })
+                },
+                function(conn, callback){
+                    r.dbCreate('voting').run(conn, function(err, result){
+                        if (err){
+                            console.log('Database already created')
+                        }else{
+                            console.log('Created new databasse')
+                        }
+                        callback(null , conn)
+                    })
+                },
+                function(conn, callback){
+                    r.db('voting').tableCreate('poll').run(conn , function(err, result){
+                        conn.close()
+                        if (err){
+                            console.log('Table already created')
+                        } else {
+                            console.log('New table created  ')
+                        }
+                        callback(null, "Database is setup successfully")
+                    })
+                },
 
+            ],function(err, data){
+                console.log(data)
+            })
     }
     connectToRethinkDbServer(callback){
-
+        r.connect({
+            host : 'localhost',
+            port : 28015
+        }, function(err, conn){
+            callback(err, connection)
+        })
     }
     connectToDb(callback){
-
+        r.connect({
+            host : 'localhost',
+            port : 28015,
+            db : 'voting'
+        }, function(err, conn){
+            callback(err, connection)
+        })
     }
 }
 
